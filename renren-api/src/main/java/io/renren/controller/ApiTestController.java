613fbc92ff8a5af62b40c1e43990f5de9975834f
hawkroc <yinpengroc@gmail.com>
2018-02-20 18:18:57 +1300

    test

--- renren-api/src/main/java/io/renren/controller/ApiTestController.java
+++ renren-api/src/main/java/io/renren/controller/ApiTestController.java
@@ -1,12 +1,16 @@
 package io.renren.controller;
 
-
 import io.renren.annotation.Login;
 import io.renren.annotation.LoginUser;
 import io.renren.common.utils.R;
 import io.renren.entity.UserEntity;
+import io.renren.service.TransactionsService;
+import io.renren.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +19,11 @@ import springfox.documentation.annotations.ApiIgnore;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 
 /**
  * 测试interface
@@ -29,42 +34,58 @@ import javax.servlet.http.HttpSession;
  */
 @RestController
 @RequestMapping("/api")
-@Api(tags="测试interface")
+@Api(tags = "测试interface")
 public class ApiTestController {
 
-    @Login
-    @GetMapping("userInfo")
-    @ApiOperation(value="获取 Users信息", response=UserEntity.class)
-    public R userInfo(@ApiIgnore @LoginUser UserEntity user){
-        return R.ok().put("user", user);
-    }
-    
-  
-    
-    
+	@Autowired
+	private TransactionsService transactionsService;
 
-    @Login
-    @GetMapping("userId")
-    @ApiOperation("获取 UsersID")
-    public R userInfo(@ApiIgnore @RequestAttribute("userId") Integer userId){
-        return R.ok().put("userId", userId);
-    }
-
-	@RequestMapping(value = { "/version", "/versiontest" }, method = RequestMethod.GET)
-	@ResponseBody 
-    public R getVersion(HttpServletResponse rs) {
-    	rs.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        return R.ok().put("msg", "version:1.1");
+	@Login
+	@GetMapping("userInfo")
+	@ApiOperation(value = "获取 Users信息", response = UserEntity.class)
+	public R userInfo(@ApiIgnore @LoginUser UserEntity user) {
+		return R.ok().put("user", user);
 	}
-    
-    
-    @GetMapping("notToken")
-    @ApiOperation("忽略Token验证测试")
-    
-    public R notToken(HttpServletResponse rs){
-    	rs.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-    	//rs.setStatus(sc);
-        return R.ok().put("msg", "withour token");
-    }
+
+	@Login
+	@GetMapping("userId")
+	@ApiOperation("获取 UsersID")
+	public R userInfo(@ApiIgnore @RequestAttribute("userId") Integer userId) {
+		return R.ok().put("userId", userId);
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = { "/version" }, method = RequestMethod.GET)
+	@ResponseBody
+	public R getVersion(HttpServletResponse rs) {
+		rs.setStatus(HttpServletResponse.SC_OK);
+		return R.ok().put("msg", "version:1.1");
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = { "/balance/{address}" }, method = RequestMethod.GET)
+	@ResponseBody
+	public R getBalanceByAddress(HttpServletResponse rs) {
+		rs.setStatus(HttpServletResponse.SC_OK);
+		return R.ok().put("msg", "version:1.1");
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = { "/blockNumber" }, method = RequestMethod.GET)
+	@ResponseBody
+	public R getblockNumber(HttpServletResponse rs) {
+		rs.setStatus(HttpServletResponse.SC_OK);
+		Map<String, Object> map = transactionsService.getCurrentBlockNumber();
+		return R.ok(map).put("msg", "this is success");
+	}
+
+	@GetMapping("notToken")
+	@ApiOperation("忽略Token验证测试")
+
+	public R notToken(HttpServletResponse rs) {
+		rs.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		// rs.setStatus(sc);
+		return R.ok().put("msg", "withour token");
+	}
 
 }
