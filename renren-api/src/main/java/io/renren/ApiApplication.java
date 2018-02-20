0f645a491f58d7b89a304d224907309d98ef5aed
hawkroc <yinpengroc@gmail.com>
2018-02-21 12:17:44 +1300

    add blockNumber

--- renren-api/src/main/java/io/renren/ApiApplication.java
+++ renren-api/src/main/java/io/renren/ApiApplication.java
@@ -1,17 +1,18 @@
 package io.renren;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.renren.config.ApiYmlConfig;
 
 
 @SpringBootApplication
+@EnableScheduling
 @MapperScan(basePackages = {"io.renren.dao"})
 @EnableConfigurationProperties({ApiYmlConfig.class}) 
 public class ApiApplication extends SpringBootServletInitializer {
--- renren-api/src/main/java/io/renren/service/impl/TransactionsServiceImpl.java
+++ renren-api/src/main/java/io/renren/service/impl/TransactionsServiceImpl.java
@@ -1,26 +1,23 @@
 package io.renren.service.impl;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
 
-import okhttp3.FormBody;
-import okhttp3.FormBody.Builder;
+import io.renren.common.utils.StringUtil;
+import io.renren.config.ApiConfig;
+import io.renren.entity.MutilEntity;
+import io.renren.service.TransactionsService;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
-import io.renren.entity.MutilEntity;
-import io.renren.entity.TransactionEntity;
-import io.renren.service.TransactionsService;
-import io.renren.config.ApiConfig;
 
 @Service("transactionsService")
 public class TransactionsServiceImpl implements TransactionsService {
@@ -40,19 +37,27 @@ public class TransactionsServiceImpl implements TransactionsService {
 	@Override
 	public Map<String, Object> getCurrentBlockNumber() {
 		Map<String, Object> map = new HashMap<>(1);
-		String str = getResponseFromRemote(ApiConfig.getInstance().getEth_blockNumber());
-		MutilEntity mutilEntity=null;
-		if(StringUtils.isNotBlank(str)){
-			//JsonUtil(str,MutilEntity.class);
-			System.out.println(str);
-			 mutilEntity=	 (MutilEntity) JSON.parseArray(str, MutilEntity.class);
-			map.put("blockNumber", mutilEntity.getResult() );
-		
-		
-			ApiConfig.getInstance().setBlockNumber(	Integer.parseInt(mutilEntity.getResult(),16));
-		}	
-		//System.out.println(st[3]);
+	      this.getCurrentBkSave();
+		map.put("blockNumber", ApiConfig.getInstance().getBlockNumber());
+    
 		return map;
+
+	}
+
+	@Scheduled(fixedDelay = 12000)
+	public void getCurrentBkSave() {
+
+		String str = getResponseFromRemote(ApiConfig.getInstance().getEth_blockNumber());
+		MutilEntity mutilEntity = null;
+		if (StringUtils.isNotBlank(str)) {
+
+			mutilEntity = JSON.parseObject(str, MutilEntity.class);
+
+			String oString = StringUtil.getRidOfPro(mutilEntity.getResult(), "0x");
+
+			ApiConfig.getInstance().setBlockNumber(Integer.parseInt(oString, 16));
+
+		}
 
 	}
 
@@ -61,8 +66,7 @@ public class TransactionsServiceImpl implements TransactionsService {
 		// TODO Auto-generated method stub
 		Map<String, Object> map = new HashMap<>(1);
 		String rs = getResponseFromRemote(ApiConfig.getInstance().getBalance(address));
-		
-		
+
 		return map;
 	}
 
@@ -71,7 +75,7 @@ public class TransactionsServiceImpl implements TransactionsService {
 		OkHttpClient client = new OkHttpClient.Builder().readTimeout(60000, TimeUnit.MILLISECONDS)
 				.writeTimeout(60000, TimeUnit.MILLISECONDS).build();
 		Request request = new Request.Builder().url(url).get().build();
-        String rString=null;
+		String rString = null;
 		Response response = null;
 		try {
 
@@ -79,18 +83,19 @@ public class TransactionsServiceImpl implements TransactionsService {
 
 			if (response != null && response.isSuccessful()) {
 				// throw new IOException("服务器端错误: " + response);
-			
-				rString=response.body().string();
+
+				rString = response.body().string();
 			} else {
 				return null;
 			}
 
-//			if (response != null && response.code() != 200) {
-//				System.out.println(Thread.currentThread().getName() + response.code());
-//				// System.out.println(response.message());
-//				System.out.println(response.body().string());
-//				// System.out.println(Info.getErrorTimes() + Info.getN());
-//			}
+			// if (response != null && response.code() != 200) {
+			// System.out.println(Thread.currentThread().getName() +
+			// response.code());
+			// // System.out.println(response.message());
+			// System.out.println(response.body().string());
+			// // System.out.println(Info.getErrorTimes() + Info.getN());
+			// }
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
