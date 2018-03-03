a0a6f46ea1f42b9b29f8fd8fb0dfb6bdde7f9337
macbook <hawkroc>
2018-03-03 21:36:30 +1300

    update springboot

--- renren-admin/src/main/java/io/renren/datasources/DynamicDataSourceConfig.java
+++ renren-admin/src/main/java/io/renren/datasources/DynamicDataSourceConfig.java
@@ -1,14 +1,17 @@
 package io.renren.datasources;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * 配置多数据源
@@ -29,6 +32,13 @@ public class DynamicDataSourceConfig {
 //    @ConfigurationProperties("spring.datasource.druid.second")
 //    public DataSource secondDataSource(){
 //        return DruidDataSourceBuilder.create().build();
+//    }
+	
+	
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+//    public DataSource firstDataSource() throws SQLException {
+//        return  new HikariDataSource();
 //    }
 
     @Bean
