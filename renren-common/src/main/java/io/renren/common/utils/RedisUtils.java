ce39f2fe2755a396149927d8bdd6772845b464a8
macbook <hawkroc>
2018-03-04 17:20:36 +1300

    update springboot

--- renren-common/src/main/java/io/renren/common/utils/RedisUtils.java
+++ renren-common/src/main/java/io/renren/common/utils/RedisUtils.java
@@ -16,13 +16,20 @@
 
 package io.renren.common.utils;
 
-import com.alibaba.fastjson.JSON;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.*;
-import org.springframework.stereotype.Component;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
 
 /**
  * Redis工具类
@@ -33,77 +40,84 @@ import java.util.concurrent.TimeUnit;
  */
 @Component
 public class RedisUtils {
-    @Autowired
-    private RedisTemplate redisTemplate;
-    @Resource(name="redisTemplate")
-    private ValueOperations<String, String> valueOperations;
-    @Resource(name="redisTemplate")
-    private HashOperations<String, String, Object> hashOperations;
-    @Resource(name="redisTemplate")
-    private ListOperations<String, Object> listOperations;
-    @Resource(name="redisTemplate")
-    private SetOperations<String, Object> setOperations;
-    @Resource(name="redisTemplate")
-    private ZSetOperations<String, Object> zSetOperations;
-    /**  默认过期时长，单位：秒 */
-    public final static long DEFAULT_EXPIRE = 60 * 60 * 24;
-    /**  不 config 过期时长 */
-    public final static long NOT_EXPIRE = -1;
+	@Autowired
+	private RedisTemplate redisTemplate;
+	@Resource(name = "redisTemplate")
+	private ValueOperations<String, String> valueOperations;
+	@Resource(name = "redisTemplate")
+	private HashOperations<String, String, Object> hashOperations;
+	@Resource(name = "redisTemplate")
+	private ListOperations<String, Object> listOperations;
+	@Resource(name = "redisTemplate")
+	private SetOperations<String, Object> setOperations;
+	@Resource(name = "redisTemplate")
+	private ZSetOperations<String, Object> zSetOperations;
+	/** 默认过期时长，单位：秒 */
+	public final static long DEFAULT_EXPIRE = 60 * 60 * 24;
+	/** 不 config 过期时长 */
+	public final static long NOT_EXPIRE = -1;
 
-    public void set(String key, Object value, long expire){
-        valueOperations.set(key, toJson(value));
-        if(expire != NOT_EXPIRE){
-            redisTemplate.expire(key, expire, TimeUnit.SECONDS);
-        }
-    }
+	public void set(String key, Object value, long expire) {
+		// valueOperations.set(key, toJson(value));
+		String json = null;
+		if (value != null) {
+			json = toJson(value);
+		}
 
-    public void set(String key, Object value){
-        set(key, value, DEFAULT_EXPIRE);
-    }
+		if (expire != NOT_EXPIRE) {
+			// redisTemplate.expire(key, expire, TimeUnit.SECONDS);
+			valueOperations.set(key, json, expire, TimeUnit.SECONDS);
 
-    public <T> T get(String key, Class<T> clazz, long expire) {
-        String value = valueOperations.get(key);
-        if(expire != NOT_EXPIRE){
-            redisTemplate.expire(key, expire, TimeUnit.SECONDS);
-        }
-        return value == null ? null : fromJson(value, clazz);
-    }
+		}
+	}
 
-    public <T> T get(String key, Class<T> clazz) {
-        return get(key, clazz, NOT_EXPIRE);
-    }
+	public void set(String key, Object value) {
+		set(key, value, DEFAULT_EXPIRE);
+	}
 
-    public String get(String key, long expire) {
-        String value = valueOperations.get(key);
-        if(expire != NOT_EXPIRE){
-            redisTemplate.expire(key, expire, TimeUnit.SECONDS);
-        }
-        return value;
-    }
+	public <T> T get(String key, Class<T> clazz, long expire) {
+		String value = valueOperations.get(key);
+		if (expire != NOT_EXPIRE) {
+			redisTemplate.expire(key, expire, TimeUnit.SECONDS);
+		}
+		return value == null ? null : fromJson(value, clazz);
+	}
 
-    public String get(String key) {
-        return get(key, NOT_EXPIRE);
-    }
+	public <T> T get(String key, Class<T> clazz) {
+		return get(key, clazz, NOT_EXPIRE);
+	}
 
-    public void delete(String key) {
-        redisTemplate.delete(key);
-    }
+	public String get(String key, long expire) {
+		String value = valueOperations.get(key);
+		if (expire != NOT_EXPIRE) {
+			redisTemplate.expire(key, expire, TimeUnit.SECONDS);
+		}
+		return value;
+	}
 
-    /**
-     * Object转成JSON数据
-     */
-    private String toJson(Object object){
-        if(object instanceof Integer || object instanceof Long || object instanceof Float ||
-                object instanceof Double || object instanceof Boolean || object instanceof String){
-            return String.valueOf(object);
-        }
-        return JSON.toJSONString(object);
-    }
+	public String get(String key) {
+		return get(key, NOT_EXPIRE);
+	}
 
-    /**
-     * JSON数据，转成Object
-     */
-    private <T> T fromJson(String json, Class<T> clazz){
-        return JSON.parseObject(json, clazz);
-    }
+	public void delete(String key) {
+		redisTemplate.delete(key);
+	}
+
+	/**
+	 * Object转成JSON数据
+	 */
+	private String toJson(Object object) {
+		if (object instanceof Integer || object instanceof Long || object instanceof Float || object instanceof Double
+				|| object instanceof Boolean || object instanceof String) {
+			return String.valueOf(object);
+		}
+		return JSON.toJSONString(object);
+	}
+
+	/**
+	 * JSON数据，转成Object
+	 */
+	private <T> T fromJson(String json, Class<T> clazz) {
+		return JSON.parseObject(json, clazz);
+	}
 }
