690cf5569f2a670b4d19af581a0628ec9016bfc5
pengyin <yinpengroc@gmail.com>
2018-04-21 10:47:40 +1200

    fix

---
+++ renren-api/src/main/java/io/renren/service/BaseService.java
@@ -0,0 +1,10 @@
+package io.renren.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public interface BaseService {
+	public  final Logger log = LoggerFactory.getLogger(BaseService.class);
+}
