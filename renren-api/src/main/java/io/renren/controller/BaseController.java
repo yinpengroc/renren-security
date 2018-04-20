81068db010dcd3087957347c57c7e353d0a9999a
pengyin <yinpengroc@gmail.com>
2018-04-21 10:43:31 +1200

    fix

---
+++ renren-api/src/main/java/io/renren/controller/BaseController.java
@@ -0,0 +1,8 @@
+package io.renren.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface BaseController {
+	public   Logger log = LoggerFactory.getLogger(BaseController.class);
+}
