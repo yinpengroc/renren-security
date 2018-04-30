26638237ff984ef18959d8ef3e48092606b22d4c
hawkroc <yinpengroc@gmail.com>
2018-05-01 00:28:26 +1200

    login

---
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/SysAccountingController.java
@@ -0,0 +1,12 @@
+package io.renren.modules.sys.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sys/accounting")
+public class SysAccountingController extends AbstractController {
+
+	
+	
+}
